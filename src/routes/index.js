76e7bb61f116ef121f25073085bc479e3695f124
Fábio Souza <fabiobarboza7@hotmail.com>
2020-01-23 23:57:32 -0300

    update routes/index

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,14 +1,23 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 import { Switch } from 'react-router-dom';
 
-import Main from '~/pages/Main';
-
+import Dashboard from '../pages/Dashboard';
+import Home from '../pages/Home';
+import { Store } from '../store';
+import modules from '../store/modules';
 import Route from './Route';
 
+const { Provider } = Store;
+
 export default function Routes() {
+  const store = useReducer(modules, { user: { logged_in: false } });
+
   return (
-    <Switch>
-      <Route path="/" exact component={Main} />
-    </Switch>
+    <Provider value={store}>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/dashboard" component={Dashboard} isPrivate />
+      </Switch>
+    </Provider>
   );
 }
